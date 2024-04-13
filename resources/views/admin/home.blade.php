7ae6ec4c399e8c819f299d577f849d354e167c0e
nhvu2001 <vunguyendienlam@gmail.com>
2024-04-13 21:47:38 +0700

    laravel

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,6 @@
+@extends('admin.main')
+
+
+@section('content')
+    Noi dung
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\Users\LoginController;
+use \App\Http\Controllers\Admin\MainController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('admin/users/login', [LoginController::class, 'index'] )->name('login');
+Route::post('admin/users/login/store', [LoginController::class, 'store'] );
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::prefix('admin')->group(function (){
+        Route::get('/', [MainController::class, 'index'])->name('admin');
+        Route::get('main', [MainController::class, 'index']);
+
+        Route::prefix('menus')->group(function () {
+            Route::get('add', [MenuController::class, 'create']);
+            Route::post('add', [MenuController::class, 'store']);
+            Route::get('list', [MenuController::class, 'index']);
+            Route::get('edit/{menu}', [MenuController::class, 'show']);
+            Route::post('edit/{menu}', [MenuController::class, 'update']);
+            Route::DELETE('destroy', [MenuController::class, 'destroy']);
+        });
+
+        #Product
+        Route::prefix('products')->group(function () {
+            Route::get('add', [ProductController::class, 'create']);
+            Route::post('add', [ProductController::class, 'store']);
+            Route::get('list', [ProductController::class, 'index']);
+            Route::get('edit/{product}', [ProductController::class, 'show']);
+            Route::post('edit/{product}', [ProductController::class, 'update']);
+            Route::DELETE('destroy', [ProductController::class, 'destroy']);
+        });
+
+        #Upload
+        Route::post('upload/services', [\App\Http\Controllers\Admin\UploadController::class, 'store']);
+    });
+});
+
+
